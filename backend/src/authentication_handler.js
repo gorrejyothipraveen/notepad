666f89838cadfcf6b6d357773689177c246a27c5
Jyothi Praveen Gorre <jyothipraveengorre@Jyothis-MacBook-Pro.local>
2026-03-01 11:08:25 +0530

    implements the login page
    * takes name and set the name in the cookie

--- backend/src/authentication_handler.js
+++ backend/src/authentication_handler.js
@@ -1,3 +1,14 @@
+import { setCookie } from "hono/cookie";
+
 export const login = (c) => {
-  return c.text('OK..')
-}
+  /*
+    - take the form data
+    - retrieve the record from the users table
+    - check whether present or not
+    - all these comes under middle ware
+    - if yes then redirect to profile page
+  */
+  const username = c.get("username");
+  setCookie(c, "username", username);
+  return c.redirect("/profile", 303);
+};
--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 import { createApp } from "./backend/src/app.js";
 import { createDB } from "./database/src/common_queries.js";
-// import { insertIntoNotes } from "./database/src/notes.queries.js";
-// import { insertIntoUsers } from "./database/src/users_queries.js";
 
 const main = () => {
   const db = createDB('notepad.db');
